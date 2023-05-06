3a95a3f88911ff6c4cd4e8227c0c3aa4b4d05c4a
Raymond Oluoch <rasholuoch@gmail.com>
2023-05-06 15:36:39 +0300

    complete app

---
+++ README.md
@@ -0,0 +1,25 @@
+# React Query 3 - CRUD Book App
+
+You need to launch client and server separately.
+
+To launch client go to `client` folder and run:
+
+```
+yarn && yarn start
+```
+
+Then in a separate tab open `server` and run:
+
+```
+yarn && yarn start
+```
+
+## Technoogies
+
+- React
+- React Query
+- Node
+- Express
+- Styled Component
+- Swagger
+- Rebase
---
+++ client/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import App from "./App";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components"
+import preset from '@rebass/preset'
+
+const queryClient = new QueryClient();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={preset}>
+        <BrowserRouter>
+          <App />
+          <ReactQueryDevtools  position="bottom-right" />
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
